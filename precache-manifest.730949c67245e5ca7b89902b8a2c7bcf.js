279408fb37da411a44c798b4d20b0c746b1b8aec
Gaychey <tgaychey@gmail.com>
2019-04-19 09:15:10 -0400

    Updates

---
+++ precache-manifest.730949c67245e5ca7b89902b8a2c7bcf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  },
+  {
+    "revision": "c68c2a9468f15ccd81d3",
+    "url": "/todo/static/js/main.efa328cd.chunk.js"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/todo/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "1411a1525dc6ff41ee1e9b484da58802",
+    "url": "/todo/index.html"
+  }
+];
