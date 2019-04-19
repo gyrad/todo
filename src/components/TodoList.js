c818e9bfd4b31fc3e16d1dd3b08ff304a3e402ac
Gaychey <tgaychey@gmail.com>
2019-04-19 12:50:09 -0400

    Fixed checkbox issue. Checkbox is now controlled.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,25 +5,26 @@ const TodoList = ({ items, remove, toggleCompleted }) => {
     let renderedList = <div className="content">
         <ul style={{ listStyle: 'none' }}>
             {items.map((item, index) => (
-                <li key={index}>
-                    <label>
-                        <input type="checkbox" onChange={() => toggleCompleted(item.id, item.todo)} /> &nbsp;
-                        <span 
-                            className="is-size-5" 
-                            style={{ textDecoration: item.completed ? 'line-through' : 'none' }}
+                    <li key={index}>
+                        <label>
+                            <input type="checkbox" checked={item.completed} onChange={() => toggleCompleted(item.id, item.todo)} /> &nbsp;
+                        <span
+                                className="is-size-5"
+                                style={{ textDecoration: item.completed ? 'line-through' : 'none' }}
+                            >
+                                {item.todo}
+                            </span>
+                        </label>
+                        <button
+                            className="button is-danger is-small is-rounded is-outlined"
+                            style={{ marginLeft: '.5rem' }}
+                            onClick={() => remove(item.id)}
                         >
-                            {item.todo}
-                        </span>
-                    </label>
-                    <button 
-                        className="button is-danger is-small is-rounded is-outlined" 
-                        style={{ marginLeft: '.5rem' }} 
-                        onClick={() => remove(item.id)}
-                    >
-                        Delete &times;
+                            Delete &times;
                     </button>
-                </li>
-            ))}
+                    </li>
+                )
+            )}
         </ul>
     </div>;
 
